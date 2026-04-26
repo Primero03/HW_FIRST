b8155940e8c7c7f4d5fb09b388707f0e9a323eb0
Primero03 <116756259+Primero03@users.noreply.github.com>
2026-04-26 18:01:29 +0300

    ARR2D

--- hw2/Main.java
+++ hw2/Main.java
@@ -1,11 +1,48 @@
-package hw2;
+import java.util.Random;
 
 public class Main {
+    public static final int SIZE = 8;
+
     public static void main(String[] args) {
-        System.out.println("Denis Gladkov");
+        int[][] colors = filling(SIZE);
+        System.out.println("_____________________________________");
+        printRotated(SIZE, colors);
+
+    }
+
+    
+
+    public static int[][] filling(int size) {
+        Random rnd = new Random();
+        int[][] matrix = new int[size][size];
+        for (int i = 0; i < size; i++) {
+            System.out.print("[");
+            for (int j = 0; j < size; j++) {
+                int z = rnd.nextInt(255);
+                matrix[i][j] = z;
+                System.out.format("%4d", matrix[i][j]);
+            }
+            System.out.print(" ]");
+            System.out.println();
         }
+        return matrix;
+    }
+
+    public static void printRotated(int size, int[][] basic) {
+        int[][] rotated = new int[size][size];
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                rotated[j][size - 1 - i] = basic[i][j];
+            }
+        }
+        for (int[] row : rotated) {
+            System.out.print("[");
+            for (int cell : row) {
+                System.out.format("%4d", cell);
+            }
+            System.out.print(" ]");
+            System.out.println();
+
+        }
+    }
 }
-
-
-
-
