dbd4ad546a68bb217dc7492757598b3261e847d2
Primero03 <116756259+Primero03@users.noreply.github.com>
2026-04-26 19:46:53 +0300

    Update Main.java

--- hw2/Main.java
+++ hw2/Main.java
@@ -5,44 +5,46 @@ public class Main {
 
     public static void main(String[] args) {
         int[][] colors = filling(SIZE);
+        print(colors);
         System.out.println("_____________________________________");
-        printRotated(SIZE, colors);
+        int[][] rotatedColors = rotated(SIZE, colors);
+        print(rotatedColors);
+
+        rotated(SIZE, colors);
+
 
     }
 
-    
-
-    public static int[][] filling(int size) {
-        Random rnd = new Random();
-        int[][] matrix = new int[size][size];
-        for (int i = 0; i < size; i++) {
-            System.out.print("[");
-            for (int j = 0; j < size; j++) {
-                int z = rnd.nextInt(255);
-                matrix[i][j] = z;
-                System.out.format("%4d", matrix[i][j]);
-            }
-            System.out.print(" ]");
-            System.out.println();
-        }
-        return matrix;
-    }
-
-    public static void printRotated(int size, int[][] basic) {
-        int[][] rotated = new int[size][size];
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
-                rotated[j][size - 1 - i] = basic[i][j];
-            }
-        }
-        for (int[] row : rotated) {
+    public static void print(int[][] matrix) {
+        for (int[] row : matrix) {
             System.out.print("[");
             for (int cell : row) {
                 System.out.format("%4d", cell);
             }
             System.out.print(" ]");
             System.out.println();
-
         }
+    }
+
+    public static int[][] filling(int size) {
+        Random rnd = new Random();
+        int[][] matrix = new int[size][size];
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                int z = rnd.nextInt(255);
+                matrix[i][j] = z;
+            }
+        }
+        return matrix;
+    }
+
+    public static int[][] rotated(int size, int[][] basic) {
+        int[][] rotated = new int[size][size];
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                rotated[j][size - 1 - i] = basic[i][j];
+            }
+        }
+        return rotated;
     }
 }
